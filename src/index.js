98511ac0404abd581b5a8d5c08a2161fe8c89a76
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2024-07-18 11:51:45 +0300

    feat: add translation of numbers up to 10 and from 10 to 20

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ module.exports = function toReadable (number) {
     const singular = ["zero", "one", "two", "three", "fourth", "five", "six", "seven", "eighth", "nine"];
     const binary = ["ten", "eleven", "twelwe", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen"];
     const dozens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const hundredths = ["hundred", "two hundred", "three hundred", "fourth hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"]
+    const hundredths = ["hundred", "two hundred", "three hundred", "fourth hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
+
+    let result = "";
+    let num = String(number)
+
+    if (num === "0") {
+        return singular[0]
+    }
+
+    if (num.length === 1) {
+        result = singular[num]
+    } else if (num.length === 2) {
+        if (num < 20) {
+            result = binary[num - 10]
+        }
+    }
+
+
+
+    return result;
 }
