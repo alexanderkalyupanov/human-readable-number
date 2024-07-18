9c7a477d3dde3180b08b3c842d3467190ca06a61
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2024-07-18 11:32:24 +0300

    feat: add variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    const singular = ["zero", "one", "two", "three", "fourth", "five", "six", "seven", "eighth", "nine"];
+    const binary = ["ten", "eleven", "twelwe", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const dozens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hundredths = ["hundred", "two hundred", "three hundred", "fourth hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"]
 }
