056c7b253c5d617a5aff1a7ecd0eab54de7e85bc
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2024-07-18 12:17:04 +0300

    feat: fixed errors with variables in arrays, added translation of 3-digit numbers

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 module.exports = function toReadable (number) {
-    const singular = ["zero", "one", "two", "three", "fourth", "five", "six", "seven", "eighth", "nine"];
-    const binary = ["ten", "eleven", "twelwe", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    const dozens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const hundredths = ["hundred", "two hundred", "three hundred", "fourth hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
+    const singular = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const binary = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const dozens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hundredths = ["", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
 
     let result = "";
     let num = String(number)
 
-    if (num === "0") {
+    if (number === "0") {
         return singular[0]
     }
 
     if (num.length === 1) {
-        result = singular[num]
+        result = singular[number]
     } else if (num.length === 2) {
         if (num < 20) {
-            result = binary[num - 10]
+            result = binary[number - 10]
         } else {
-            result = dozens[Math.floor(num / 10)] + (num % 10 !== 0 ? " " + singular[num % 10] : "")
+            result = dozens[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + singular[number % 10] : "")
         }
+    } else if (num.length === 3) {
+        result = hundredths[Math.floor(number / 100)] + " " + toReadable(number % 100)
     }
+
 
 
     return result;
